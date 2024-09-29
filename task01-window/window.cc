05c957e730366a3891d314c37ed141494737b9e6
Thomas Wilde <ThomasWilde1984@gmail.com>
2024-09-29 14:05:43 +0200

    add: task01-window, task02-clear_color

---
+++ task01-window/window.cc
@@ -0,0 +1,45 @@
+#include <iostream>   // used for console output
+
+// ToDo: include necessary headers for OpenGL (GLEW, GLFW)
+
+int
+main(void) {
+  std::cout << "Task01 - Initialize a Render Window" << std::endl;
+
+  // ToDo step 1
+  // - initialize GLFW
+  // - exit if initialization fails
+
+  // ToDo step 2
+  // - set the properties of GLFW
+  // - us OpenGL version
+  // - use the core profile, i.e. no backwards compatibility
+
+  // ToDo step 3
+  // - create a window
+  // - 800 x 600 pixels
+  // - exit if window creation fails
+
+  // ToDo step 4
+  // - read the buffer size (window dimensions)
+  // - set the render context to the current window
+
+  // ToDo step 5
+  // - setup GLEW
+  // - allow GLEW to use experimental functions
+  // - clean up and exit if this fails
+
+  // ToDo step 6
+  // - set up the viewport with the buffer size
+
+  // ToDo step 7
+  // - run a loop in which you clear the viewport
+  // - set the clear color to a value of your choice
+  // - swap front an back buffer to show the result
+
+  // ToDo step 8
+  // - clean everything up
+  // - exit the program
+
+  return 0;
+}
---
+++ task02-clear_color/clear_color.cc
@@ -0,0 +1,21 @@
+#include <iostream>   // used for console output
+
+// ToDo: include necessary headers for OpenGL (GLEW, GLFW)
+
+int
+main(void) {
+  std::cout << "Task02 - Change Clear Color" << std::endl;
+
+  // ToDo step 1 - 6
+  // - repeat steps from task01-window
+
+  // ToDo step 7
+  // - perform a smooth color change of the clear color in the render loop
+  // - e.g. select a nice color palette from ColorBrewer and perform a linear
+  //   interpolation between the colors
+
+  // ToDo step 8
+  // - repeat step from task01-window
+
+  return 0;
+}
